2753097fd7768386110d6150034c4f58a147b154
ishaib <ishaib@il.ibm.com>
2016-05-19 09:15:10 +0300

    Add Copyright / License Header

--- IBMMobileFirstPlatformFoundationLiveUpdate/app/src/androidTest/java/com/worklight/ibmMobileFirstplatformFoundationLiveUpdateTestapp/ApplicationTest.java
+++ IBMMobileFirstPlatformFoundationLiveUpdate/app/src/androidTest/java/com/worklight/ibmMobileFirstplatformFoundationLiveUpdateTestapp/ApplicationTest.java
@@ -1,8 +1,24 @@
+/**
+ *   © Copyright 2016 IBM Corp.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 package com.worklight.ibmMobileFirstplatformFoundationLiveUpdateTestapp;
 
 import android.app.Application;
 import android.content.Context;
 import android.test.ApplicationTestCase;
+import android.util.Log;
 
 import com.worklight.common.Logger;
 import com.worklight.ibmmobilefirstplatformfoundationliveupdate.LiveUpdateManager;
@@ -67,6 +83,7 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
         try {
             Thread.sleep(10000);
         } catch (InterruptedException e) {
+            Log.i(getName(), "Forced InterruptedException to allow continue the test");
         }
 
         assertConfig();
@@ -92,6 +109,7 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
         try {
             Thread.sleep(10000);
         } catch (InterruptedException e) {
+            Log.i(getName(), "Interruption forced");
         }
 
         assertConfig ();
@@ -118,6 +136,7 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
         try {
             Thread.sleep(10000);
         } catch (InterruptedException e) {
+            Log.i(getName(), "Forced InterruptedException to allow continue the test");
         }
 
         assertEquals(404,testFailResponse.getStatus());
