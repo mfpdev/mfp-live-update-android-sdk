ab3bebc3ff719fb17bc60e3a6f70748c1c41c6d1
ishaib <ishaib@il.ibm.com>
2016-06-29 16:50:29 +0300

    Test non-English characters

--- IBMMobileFirstPlatformFoundationLiveUpdate/app/src/androidTest/java/com/worklight/ibmMobileFirstplatformFoundationLiveUpdateTestapp/ApplicationTest.java
+++ IBMMobileFirstPlatformFoundationLiveUpdate/app/src/androidTest/java/com/worklight/ibmMobileFirstplatformFoundationLiveUpdateTestapp/ApplicationTest.java
@@ -39,8 +39,8 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     }
 
     Context context;
-    Configuration testConfig1;
-    Configuration testConfig2;
+
+    Configuration [] configs;
     WLFailResponse testFailResponse;
 
     public void setUp() throws Exception {
@@ -50,29 +50,40 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
 
         WLClient.createInstance(context);
         Logger.setContext(context);
-        testConfig1 = null;
-        testConfig2 = null;
+        configs = new Configuration[4];
         testFailResponse = null;
     }
 
-    private void assertConfig2 () {
-        assertEquals(testConfig2.getProperty("property1"), "value11");
-        assertEquals(testConfig2.getProperty("property2"), "value22");
-        assertEquals(testConfig2.isFeatureEnabled("feature1"), Boolean.FALSE);
-        assertEquals(testConfig2.isFeatureEnabled("feature2"), Boolean.TRUE);
+    private void assertConfig1 () {
+        assertEquals(configs[0].getProperty("property1"), "value1");
+        assertEquals(configs[0].getProperty("property2"), "value2");
+        assertEquals(configs[0].isFeatureEnabled("feature1"), Boolean.TRUE);
+        assertEquals(configs[0].isFeatureEnabled("feature2"), Boolean.FALSE);
 
-        assertNull(testConfig2.getProperty("notExistProperty"));
-        assertNull(testConfig2.getProperty("featureNotExist"));
+        assertNull(configs[0].getProperty("notExistProperty"));
+        assertNull(configs[0].getProperty("featureNotExist"));
     }
 
-    private void assertConfig1 () {
-        assertEquals(testConfig1.getProperty("property1"), "value1");
-        assertEquals(testConfig1.getProperty("property2"), "value2");
-        assertEquals(testConfig1.isFeatureEnabled("feature1"), Boolean.TRUE);
-        assertEquals(testConfig1.isFeatureEnabled("feature2"), Boolean.FALSE);
+    private void assertConfig2 () {
+        assertEquals(configs[1].getProperty("property1"), "value11");
+        assertEquals(configs[1].getProperty("property2"), "value22");
+        assertEquals(configs[1].isFeatureEnabled("feature1"), Boolean.FALSE);
+        assertEquals(configs[1].isFeatureEnabled("feature2"), Boolean.TRUE);
 
-        assertNull(testConfig1.getProperty("notExistProperty"));
-        assertNull(testConfig1.getProperty("featureNotExist"));
+        assertNull(configs[1].getProperty("notExistProperty"));
+        assertNull(configs[1].getProperty("featureNotExist"));
+    }
+
+    private void assertConfig3 () {
+        assertEquals(configs[2].getProperty("property1"), "Çàâｱｲｳ");
+        assertEquals(configs[2].getProperty("property2"), "ÇàâｱｲｳÇàâｱｲｳ");
+        assertEquals(configs[2].isFeatureEnabled("feature1"), Boolean.TRUE);
+        assertEquals(configs[2].isFeatureEnabled("feature2"), Boolean.TRUE);
+    }
+
+    private void assertConfig4 () {
+        assertEquals(configs[3].getProperty("ÇàâｱｲｳÇàâｱ ｲｳ"), "ÇàâｱｲｳÇàâｱ ｲｳ ÇàâｱｲｳÇàâｱ ｲｳ");
+        assertEquals(configs[3].isFeatureEnabled("Çàâｱｲ ｳÇàâｱｲｳ"), Boolean.TRUE);
     }
 
     public void testObtainConfigurationWithParams() throws Exception {
@@ -83,7 +94,7 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
         LiveUpdateManager.getInstance().obtainConfiguration(params, false, new ConfigurationListener() {
             @Override
             public void onSuccess(final Configuration configuration) {
-                testConfig2 = configuration;
+                configs[1] = configuration;
                 currentThread.interrupt();
             }
 
@@ -104,12 +115,41 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
         assertConfig2();
     }
 
+    public void testObtainConfigurationWithNonEnglishAndSpaceParams() throws Exception {
+        final Thread currentThread = Thread.currentThread();
+        Map <String, String>params = new HashMap();
+        params.put("Çà âｱｲｳ", "Çàâｱｲ ｳÇàâｱｲｳ");
+
+        LiveUpdateManager.getInstance().obtainConfiguration(params, false, new ConfigurationListener() {
+            @Override
+            public void onSuccess(final Configuration configuration) {
+                configs[3] = configuration;
+                currentThread.interrupt();
+            }
+
+            @Override
+            public void onFailure(WLFailResponse wlFailResponse) {
+                currentThread.interrupt();
+            }
+        });
+
+
+        //Wait 10 seconds for response in maximum
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            Log.i(getName(), "Forced InterruptedException to allow continue the test");
+        }
+
+        assertConfig4();
+    }
+
     public void testObtainConfigurationWithSegment() throws Exception {
         final Thread currentThread = Thread.currentThread();
         LiveUpdateManager.getInstance().obtainConfiguration("segment1", new ConfigurationListener() {
             @Override
             public void onSuccess(final Configuration configuration) {
-                testConfig1 = configuration;
+                configs[0] = configuration;
                 currentThread.interrupt();
             }
 
@@ -128,6 +168,32 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
         }
 
         assertConfig1 ();
+    }
+
+    public void testObtainConfigurationWithNonEnglishCharacterSegment() throws Exception {
+        final Thread currentThread = Thread.currentThread();
+        LiveUpdateManager.getInstance().obtainConfiguration("Çàâｱｲｳ", false, new ConfigurationListener() {
+            @Override
+            public void onSuccess(final Configuration configuration) {
+                configs[2] = configuration;
+                currentThread.interrupt();
+            }
+
+            @Override
+            public void onFailure(WLFailResponse wlFailResponse) {
+                currentThread.interrupt();
+            }
+        });
+
+
+        //Wait 10 seconds for response in maximum
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            Log.i(getName(), "Interruption forced");
+        }
+
+        assertConfig3 ();
     }
 
     public void testObtainNonExitingSegment() throws Exception {
